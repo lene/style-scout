29152c721a3c02f6dd6e56c0eb7a095a8a285427
Lene Preuss <lene.preuss@gmail.com>
2017-05-05 23:31:02 +0200

    option to display only liked items stop indefinite growing of file name

--- like_items.py
+++ like_items.py
@@ -25,40 +25,29 @@ untouched), 'b' to view the previous item and 's' or 'q' to stop the program."""
         '--item-file', default=None, help="Pickle file from which to load downloaded items"
     )
     parser.add_argument(
-        '--likes-file', help="JSON file containing the liked item IDs"
+        '--liked-only', action='store_true', help="Only display liked items"
     )
-    parser.add_argument('--size', type=int, default=400)
+    parser.add_argument('--size', type=int, default=400, help="Size of the displayed images")
 
     return parser.parse_args()
 
 
 class LikeItemsUI:
 
-    def __init__(self, app, items, start, size):
+    def __init__(self, app, items, start, size, liked_only=False):
         self.item_no = start
-        self.items = items
+        self.items = [i for i in items if '<3' in i.tags] if liked_only else items
         self.app = app
         self.image_size = (size, size)
         self.liked = len([i for i in self.items[:start] if '<3' in i.tags])
 
-        self.app.addLabel('title', 'Like or dislike eBay items here')
+        self._setup_ui()
 
-        self.app.startLabelFrame("Simple", 0, 0)
-        for image_no in range(4):
-            image = Image.new('RGB', self.image_size)
-            image.save('/tmp/i{}.gif'.format(image_no))
-            self.app.addImage(
-                'image_{}'.format(image_no), '/tmp/i{}.gif'.format(image_no), image_no // 2, image_no % 2
-            )
-        self.app.stopLabelFrame()
+        self._setup_key_bindings()
 
-        self.app.addLabel('item_no', '{}/{}'.format(self.item_no, len(self.items)))
+        self.update_content()
 
-        self.app.addListBox('tags', [])
-        self.app.setListBoxRows('tags', 8)
-
-        self.app.addButtons(["Like", "Dislike", 'Stop'], self.press, 3, 0, 2)  # Row 3,Column 0,Span 2
-
+    def _setup_key_bindings(self):
         self.app.bindKey('l', self.like)
         self.app.bindKey('d', self.dislike)
         self.app.bindKey('s', self.stop)
@@ -66,7 +55,20 @@ class LikeItemsUI:
         self.app.bindKey('b', self.back)
         self.app.bindKey('n', self.next)
 
-        self.update_content()
+    def _setup_ui(self):
+        self.app.addLabel('title', 'Like or dislike eBay items here')
+        self.app.startLabelFrame("Pictures", 0, 0)
+        for image_no in range(4):
+            image = Image.new('RGB', self.image_size)
+            image.save('/tmp/i{}.gif'.format(image_no))
+            self.app.addImage(
+                'image_{}'.format(image_no), '/tmp/i{}.gif'.format(image_no), image_no // 2, image_no % 2
+            )
+        self.app.stopLabelFrame()
+        self.app.addLabel('item_no', '{}/{}'.format(self.item_no, len(self.items)))
+        self.app.addListBox('tags', [])
+        self.app.setListBoxRows('tags', 8)
+        self.app.addButtons(["Like", "Dislike", 'Stop'], self.press, 3, 0, 2)  # Row 3,Column 0,Span 2
 
     def downscale(self, image):
         w, h = image.size
@@ -99,8 +101,7 @@ class LikeItemsUI:
         self.update_content()
 
     def next(self, _):
-        self.item_no += 1
-        self.update_content()
+        self.next_item()
 
     def next_item(self):
         self.item_no += 1
@@ -137,11 +138,11 @@ with open(item_file_name, 'rb') as pickle_file:
 try:
     app = gui()
 
-    ui = LikeItemsUI(app, items, start, args.size)
+    ui = LikeItemsUI(app, items, start, args.size, liked_only=args.liked_only)
 
     app.go()
 finally:
-    item_file_name += '.{}'.format(ui.item_no)
+    item_file_name = '.'.join(item_file_name.split('.')[:2] + [str(ui.item_no)])
     with open(join(item_file_name), 'wb') as pickle_file:
         dump(items, pickle_file)
 
