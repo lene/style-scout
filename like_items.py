65e8d59dbad3e6a69d685e0a00d82fe6e30cfc9e
Lene Preuss <lene.preuss@here.com>
2017-05-05 17:07:21 +0200

    save state of the UI to continue where you left off

--- like_items.py
+++ like_items.py
@@ -1,11 +1,13 @@
 from appJar import gui
 from pickle import load, dump
 from argparse import ArgumentParser
-from os.path import join
+from os.path import join, isfile
 from PIL import Image
 from pprint import pprint
 
 from data_sets.contains_images import add_border
+
+STATE_FILE = 'data/like_items.state'
 
 SAVE_FOLDER = 'data'
 
@@ -50,6 +52,10 @@ class LikeItemsUI:
 
         self.app.addButtons(["Like", "Dislike", 'Stop'], self.press, 3, 0, 2)  # Row 3,Column 0,Span 2
 
+        self.app.bindKey('l', self.like)
+        self.app.bindKey('d', self.dislike)
+        self.app.bindKey('s', self.stop)
+
         self.update_content()
 
     def downscale(self, image, size):
@@ -58,14 +64,24 @@ class LikeItemsUI:
         return image.resize(size, Image.BICUBIC)
 
     def press(self, btn):
+        print('BTN', btn)
         if btn == "Stop":
-            pprint([str(i) for i in self.items[:self.item_no]])
+            pprint([i.tags for i in self.items[:self.item_no]])
             self.app.stop()
         elif btn == 'Like':
             self.items[self.item_no].like()
             self.next_item()
         else:
             self.next_item()
+
+    def stop(self, _):
+        self.press('Stop')
+
+    def like(self, _):
+        self.press('Like')
+
+    def dislike(self, _):
+        self.press('Dislike')
 
     def next_item(self):
         self.item_no += 1
@@ -84,16 +100,27 @@ class LikeItemsUI:
             app.reloadImage('image_{}'.format(image_no), '/tmp/i{}.gif'.format(image_no))
 
 
-args = parse_command_line()
+if isfile(STATE_FILE):
+    with open(STATE_FILE, 'rb') as pickle_file:
+        params = load(pickle_file)
+    item_file_name = params['item_file']
+    start = params['start']
+else:
+    args = parse_command_line()
+    item_file_name = join(args.save_folder, args.item_file)
+    start = args.start
 
-with open(join(args.save_folder, args.item_file), 'rb') as pickle_file:
+with open(item_file_name, 'rb') as pickle_file:
     items = load(pickle_file)
 
 app = gui()
 
-ui = LikeItemsUI(app, items, args.start)
+ui = LikeItemsUI(app, items, start)
 
 app.go()
 
-with open(join(args.save_folder, args.item_file + '.{}.liked'.format(ui.item_no)), 'wb') as pickle_file:
+with open(join(item_file_name + '.{}.liked'.format(ui.item_no)), 'wb') as pickle_file:
     dump(items, pickle_file)
+
+with open(STATE_FILE, 'wb') as pickle_file:
+    dump({'item_file': item_file_name, 'start': ui.item_no}, pickle_file)
